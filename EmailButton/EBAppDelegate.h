74bf82531e90634347753c39f5797e0214d634b9
Joshua Howland <development@jkhowland.com>
2014-06-05 14:03:07 -0600

    Initial Commit

---
+++ EmailButton/EBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EBAppDelegate.h
+//  EmailButton
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EmailButtonTests/EmailButtonTests.m
@@ -0,0 +1,34 @@
+//
+//  EmailButtonTests.m
+//  EmailButtonTests
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmailButtonTests : XCTestCase
+
+@end
+
+@implementation EmailButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
