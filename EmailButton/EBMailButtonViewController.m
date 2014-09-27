46a4e129e3a7047890a62df3d889f7ea85205f10
Taylor Mott <trm36@humboldt.edu>
2014-09-27 12:19:38 -0600

    Mail -- In class project completed
    practice with presenting a new email viewcontroller
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- EmailButton/EBMailButtonViewController.m
+++ EmailButton/EBMailButtonViewController.m
@@ -7,17 +7,36 @@
 //
 
 #import "EBMailButtonViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface EBMailButtonViewController ()
+@interface EBMailButtonViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
 @implementation EBMailButtonViewController
 
-- (IBAction)sendEmail:(id)sender {
+- (IBAction)sendEmail:(id)sender
+{
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+        
+        //mail is a view controller with a delegate
+        //therefore to get to the actual delegate we want
+        //we must use mailComposeDelegate
+        mailViewController.mailComposeDelegate = self;
+        
+        [mailViewController setSubject:@"An Awesome Email"];
+        [mailViewController setMessageBody:@"Here is the body for my message." isHTML:NO];
+        
+        //can run code (block) when view controller is presented (completed)
+        [self presentViewController:mailViewController animated:NO completion:nil];
+    }
+}
 
-    
-    
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [controller dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
